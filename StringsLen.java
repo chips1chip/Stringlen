6e92dc0b015b9fa49242de2a20e81c316ea40a42
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-25 12:58:58 +0530

    Add files via upload

---
+++ StringsLen.java
@@ -0,0 +1,20 @@
+public class StringsLen{
+    public static void main(String[] args) {
+        String str1="anjali";
+        //char oldchar='i';
+        //char newchar='o';
+        //String str2=str1.replace(oldchar,newchar);
+        //System.out.println("original String:"+str1);
+        //System.out.println("modified String:"+str2);
+
+       // String str2="world";//
+       // System.out.println(str.length()); //
+       // System.out.println(str1.concat(str2));//
+      // System.out.println(str1.toUpperCase());//
+      System.out.println(str1.substring(2, 6));
+      
+       
+       
+       
+    }
+}
