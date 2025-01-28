6c4727aba1c389e45d10d66cb0966dde9262806a
Anjalinegi <anjalinegi26500@gmail.com>
2025-01-29 01:46:01 +0530

    Update StringsLen.java

--- StringsLen.java
+++ StringsLen.java
@@ -1,20 +1,17 @@
 public class StringsLen{
     public static void main(String[] args) {
-        String str1="anjali";
-        //char oldchar='i';
-        //char newchar='o';
-        //String str2=str1.replace(oldchar,newchar);
-        //System.out.println("original String:"+str1);
-        //System.out.println("modified String:"+str2);
+        String str1 = "anjali";  // Original string
+        char oldchar = 'i';  // Character to be replaced
+        char newchar = 'o';  // Character to replace with
+        String str2 = str1.replace(oldchar, newchar);  // Replace 'i' with 'o' in str1
+        System.out.println("original String: " + str1);
+        System.out.println("modified String: " + str2);
 
-       // String str2="world";//
-       // System.out.println(str.length()); //
-       // System.out.println(str1.concat(str2));//
-      // System.out.println(str1.toUpperCase());//
-      System.out.println(str1.substring(2, 6));
-      
-       
-       
-       
+        String str3 = "world";  // Another string to concatenate
+        System.out.println(str1.length());  // Print length of str1
+        System.out.println(str1.concat(str3));  // Concatenate str1 and str3
+        System.out.println(str1.toUpperCase());  // Convert str1 to uppercase
+
+        System.out.println(str1.substring(2, 6));  // Print substring from index 2 to 5 of str1
     }
 }
